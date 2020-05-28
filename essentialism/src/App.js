212077d0b9b314973ac8ef01825c90d2d6ccbc17
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-28 15:59:33 -0500

    commit

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,22 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import UserForm from "./UserForm"
-import {BrowserRouter as Router, Route} from "react-router-dom";
-import UserFormOne from './UserFormOne';
-import ValueList from './components/Values/ValueList'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UserForm from "./UserForm";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import UserFormOne from "./UserFormOne";
+import ValueList from "./components/Values/ValueList";
 
 function App() {
-  return (
-    <div className="App">
-      <Route>
-        <UserFormOne/>
-      </Route>
-      <ValueList/>
-      {/* <UserForm/> */}
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
